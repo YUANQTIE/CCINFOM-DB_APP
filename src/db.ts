4f284015e6e8687778be9e27ae5a404dbbc85e78
JR Santiago <jrsantiago3001@gmail.com>
2025-11-08 18:28:16 +0800

    fix: enironment variables

--- src/db.ts
+++ src/db.ts
@@ -1,20 +1,22 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 import process from "process";
 
+dotenv.config();
 export let pool: mysql.Connection;
 
 try {
-    pool = await mysql.createConnection({
-    host: "localhost",
-    user: "root",        // your MySQL username
-    password: "Dlsu1234!",// your MySQL password
-    database: "carnivault", // your database name
+    pool = mysql.createPool({
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASS!,
+    database: process.env.DB_NAME!,
     });
     
     console.log("Connected to MySQL database");
 }
 catch(error){
-    console.log("Failed to connect.", error);
+    console.log("Failed to connect.");
     process.exit(1);
 }
 
