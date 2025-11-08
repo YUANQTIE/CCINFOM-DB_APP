99965a7776bce9fbd83a3bb89171ae5942579f59
YUANQTIE <yuanpanlilio@gmail.com>
2025-11-09 03:00:03 +0800

    Added push and delete statements

--- src/db.ts
+++ src/db.ts
@@ -2,9 +2,11 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 import process from "process";
 import * as views from "./db_views.ts";
+import { create } from "domain";
 
 dotenv.config();
 export let pool: mysql.Connection;
+console.log(process.env.DB_HOST);
 
 try {
     pool = await mysql.createConnection({
@@ -25,3 +27,17 @@ console.log(await views.getLivestock(pool));
 console.log(await views.getMeatSelection(pool));
 console.log(await views.getDeliveries(pool));
 console.log(await views.getClients(pool));
+console.log(await views.getNutrition(pool));
+console.log(await views.getOrderLine(pool));
+console.log(await views.getSupplier(pool));
+console.log(await views.getAgreements(pool));
+
+
+const result1 = await views.createSupplier(pool, {
+    supplier_id: "BYM",
+    company_name: "beatyomeat",
+    contact_no: "0933-532-4345",
+})
+console.log("Deleted rows:", result1);
+const result2 = await views.deleteSupplier(pool, "BYM");
+console.log("Deleted rows:", result2);
