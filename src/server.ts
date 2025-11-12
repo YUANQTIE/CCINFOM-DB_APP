2b2264edb9b8a24802f6e1db8735c4a0856b4b49
JR Santiago <jrsantiago3001@gmail.com>
2025-11-13 00:18:46 +0800

    feat: meat selection and livestock views

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,18 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
-import { pool } from "./db.ts";
-import * as read from "./db_read.ts";
+import meatSelectionRoutes from "./routes/meatSelectionRoutes.ts";
+import livestockRoutes from "./routes/livestockRoutes.ts"
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors())
+app.use(express.static("public"));
 
-app.get("/api/meat-selection", async (req, res) => {
-  try {
-    const data = await read.getMeatSelection()
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({error: "Failed to fetch meat selection"})
-  }
-});
-
-/*
-app.use(express.static(
-    path.join(__dirname), 
-    {extensions: ["html"],})
-);
-app.use(express.static(path.join(__dirname, "../resources")));
-*/
+app.use("/api/meat-selection", meatSelectionRoutes);
+app.use("/api/livestock", livestockRoutes);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
